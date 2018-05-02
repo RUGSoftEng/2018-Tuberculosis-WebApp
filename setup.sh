bdd093a24864a0fb476e5507014774f46c64d418
StenSipma <sten.sipma@ziggo.nl>
2018-05-02 14:22:35 +0200

    Changed when dependencies are downloaded, now only downloads when setting up. Can be run by: 'make deps'

--- setup.sh
+++ setup.sh
@@ -23,6 +23,9 @@ wget -q https://dl.google.com/go/go1.10.1.linux-amd64.tar.gz \
 
 echo 'Setting PATH Variables...'
 export PROJROOT=/vagrant
+export GOPATH=/home/vagrant/go
+export PATH=$PATH:/usr/local/go/bin
+export PATH=$PATH:$GOPATH/bin
 
 echo 'Setting up paths for the vagrant ssh user'
 echo -e 'export GOPATH=/home/vagrant/go' >> /home/vagrant/.bashrc
@@ -30,6 +33,10 @@ echo -e 'export PATH=$PATH:/usr/local/go/bin' >> /home/vagrant/.bashrc
 echo -e 'export PATH=$PATH:$GOPATH/bin' >> /home/vagrant/.bashrc
 echo -e 'export PROJROOT=/vagrant' >> /home/vagrant/.bashrc
 echo 'DONE'
+
+echo 'Downloading Golang Dependencies...'
+make deps -C $PROJROOT/API && echo 'SUCCESS'
+
 
 echo 'Setting up Database...'
 mysql -u root --password=root <<< 'drop database if exists TestDB' \
