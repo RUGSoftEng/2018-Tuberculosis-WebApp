c35189003675fcb81f4353f115c24afb14b3e9ec
giacomo270197 <gcasoni@hotmail.it>
2018-05-30 12:27:17 +0200

    Add and delete meds

--- API/http.go
+++ API/http.go
@@ -77,11 +77,13 @@ func main() {
 	putRouter.Handle("/api/accounts/patients/{id:[0-9]+}/notes", handlerWrapper(createNote))
 	putRouter.Handle("/api/general/videos", handlerWrapper(createVideo))
 	putRouter.Handle("/api/admin/faq", handlerWrapper(createFAQ))
+	putRouter.Handle("/api/admin/medicines/add", handlerWrapper(pushMedicine))
 
 	// DELETE Requests for Deleting
 	deleteRouter := router.Methods("DELETE").Subrouter()
 	deleteRouter.Handle("/api/accounts/patients/{id:[0-9]+}", handlerWrapper(deletePatient))
 	deleteRouter.Handle("/api/accounts/physicians/{id:[0-9]+}", handlerWrapper(deletePhysician))
+	deleteRouter.Handle("/api/admin/medicines/{id:[0-9]+}", handlerWrapper(deleteMedicine))
 
 	// Starting the router
 	err = http.ListenAndServe(listenLocation, router)
