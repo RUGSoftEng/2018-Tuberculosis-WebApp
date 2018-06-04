8909f13bbec5f3f6ead9f2c05ef3f81c11773c98
rriesebos <rriesebos@hotmail.com>
2018-06-04 22:16:33 +0200

    Removed unwanted spaces, added different categories, fixed issue where quizzes where linked to the wrong video

--- database/video-quiz/vq-nl.sql
+++ database/video-quiz/vq-nl.sql
@@ -1,51 +1,51 @@
 insert into Videos(language, topic, title, reference) values (
        'NL',
-       'Tuberculosis',
+       'Symptomen',
        'Wat gebeurt er met mij en mijn lichaam wanneer ik tuberculose heb?',
        'https://youtu.be/_KmpFTplwRc'
 );
 
 insert into Quizzes(video, question, answers) values(
-       10,
+       11,
        'Tuberculose wordt veroorzaakt door:',
        'Een bacterie:Een virus:Een schimmel'
 );
 
 
 insert into Quizzes(video, question, answers) values(
-       10,
+       11,
        'Wat doet het lichaam om je te beschermen tegen TB?',
        'Het bouwt een soort van muur om de bacteriën:Het maakt alle bacteriën kapot.:Het verplaatst de bacteriën naar een andere plek'
 );
 
 
 insert into Quizzes(video, question, answers) values(
-       10,
+       11,
        'Hoe komt de bacterie het lichaam in',
        'Via de lucht:Via het bloed:Via voedsel:Door handen te schudden met een besmet iemand'
 );
 
 insert into Videos(language, topic, title, reference) values (
        'NL',
-       'Tuberculosis',
+       'Symptomen',
        'Tekenen dat ik tuberculose heb',
        'https://youtu.be/Ruy3mzeoQjc'
 );
 
 insert into Quizzes(video, question, answers) values(
-       11,
+       12,
        'Wat is het meest voorkomende symptoom',
        'Een hoest:Koorts:Verminderde eetlust'
 );
 
 insert into Quizzes(video, question, answers) values(
-       11,
+       12,
        'Wat moet ik doen als ik bloed ophoest',
        'Ik moet direct contact opnemen met mijn huisarts:Niets, het is niet ongebruikelijk om bloed op te hoesten:Wanneer het langer dan 3 weken duurt moet ik contact opnemen met mijn huisarts'
 );
 
 insert into Quizzes(video, question, answers) values(
-       11,
+       12,
        'Wanneer moet ik contact opnemen met mijn huisarts',
        'In beide gevallen:Wanneer ik al drie weken een droge hoest heb:Wanneer ik bloed ophoest'
 );
@@ -53,112 +53,113 @@ insert into Quizzes(video, question, answers) values(
 
 insert into Videos(language, topic, title, reference) values (
        'NL',
-       'Tuberculosis',
+       'Symptomen',
        'Wat is latente tuberculose en wanneer wordt het actieve tuberculose?',
        'https://youtu.be/KN7RWqzfZMI'
 );
 
 insert into Quizzes(video, question, answers) values(
-       12,
+       13,
        'Is latente tuberculose besmettelijk',
        'Nee:Ja'
 );
 
 insert into Quizzes(video, question, answers) values(
-       12,
+       13,
        'Wat is het gevaar van latente tuberculose',
-       'Er is een kans dat het actieve tuberculose wordt:Je kan andere mensen infecteren '
+       'Er is een kans dat het actieve tuberculose wordt:Je kan andere mensen infecteren'
 );
 
 insert into Quizzes(video, question, answers) values(
-       12,
+       13,
        'Kan het drinken van alcohol of het roken van sigaretten de kans op het krijgen van actieve tuberculose vergroten',
        'Ja:Nee'
 );
 
 insert into Videos(language, topic, title, reference) values (
        'NL',
-       'Tuberculosis',
+       'Medicatie',
        'Behandeling voor tuberculose',
        'https://youtu.be/f4ZuYCtc5A4'
 );
 
 insert into Quizzes(video, question, answers) values(
-       13,
+       14,
        'Wanneer kan je stoppen met het nemen van de medicatie?',
        'Als de dokter zegt dat ik mag stoppen:Als de symptomen zijn verdwenen:Als ik me beter voel'
 );
 
 insert into Quizzes(video, question, answers) values(
-       13,
+       14,
        'Waarom moet ik vier verschillende soorten medicatie innemen?',
-       'Omdat ze allemaal net iets anders werken:Omdat het een tijdje duurt voordat de medicatie alle bacteriën heeft gedood :Omdat ze allemaal werken in verschillende delen van je lichaam: :Waarom neem ik de antibiotica?:Alle bovenstaande antwoorden zijn juist:Om van de symptomen af te komen:Om de tuberculose bacteriën aan te vallen :Voor meer energie'
+       'Omdat ze allemaal net iets anders werken:Omdat het een tijdje duurt voordat de medicatie alle bacteriën heeft gedood:Omdat ze allemaal werken in verschillende delen van je lichaam:Waarom neem ik de antibiotica?:Alle bovenstaande antwoorden zijn juist:Om van de symptomen af te komen:Om de tuberculose bacteriën aan te vallen:Voor meer energie'
 );
 
 insert into Videos(language, topic, title, reference) values (
        'NL',
-       'Tuberculosis',
+       'Medicatie',
        'Waarom moet ik de medicatie zolang innemen?',
-       'https://youtu.be/z-23fzY9cbQ '
+       'https://youtu.be/z-23fzY9cbQ'
 );
 
 insert into Quizzes(video, question, answers) values(
-       14,
+       15,
        'Waarom duurt de behandeling zolang?',
-       'Alle bacteriën moeten worden gedood en dit duurt een tijd:De dokter denkt dat het grappig is om mij medicatie te geven voor zo’n lange periode. :Er bestaat alleen medicatie voor een halfjaar en niet voor een kortere periode.'
+       'Alle bacteriën moeten worden gedood en dit duurt een tijd:De dokter denkt dat het grappig is om mij medicatie te geven voor zo’n lange periode.:Er bestaat alleen medicatie voor een halfjaar en niet voor een kortere periode.'
 );
  
 insert into Quizzes(video, question, answers) values(
-       14,
+       15,
        'Wat kan een consequentie zijn van het niet lang genoeg innemen van de medicatie',
-       'Beide :Niet alle bacteriën worden gedood. :De bacteriën worden immuun voor de medicatie.'
+       'Beide:Niet alle bacteriën worden gedood.:De bacteriën worden immuun voor de medicatie.'
 );
 
 insert into Quizzes(video, question, answers) values(
-       14,
+       15,
        'Wanneer stop je met het innemen van de medicatie',
        'Na een periode van zes maanden:Wanneer ik geen symptomen meer heb:Wanneer de dokter mij vertelt dat ik mag stoppen'
 );
 
-
+--Maybe remove this statement since the link is wrong..
 insert into Videos(language, topic, title, reference) values (
        'NL',
-       'Tuberculosis',
+       'Medicatie',
        'Waarom verberg ik dat ik tuberculose heb?',
-       'https://youtu.be/eymOMXeSyP4 '
+       'https://youtu.be/eymOMXeSyP4'
 );
 
 insert into Videos(language, topic, title, reference) values (
        'NL',
-       'Tuberculosis',
+       'Infectie en Preventie',
        'Hoe kan ik voorkomen dat ik andere mensen infecteer?',
-       'https://youtu.be/eymOMXeSyP4 '
-);
-
-insert into Quizzes(video, question, answers) values(
-       16,
-       'Duurt de besmettelijke fase de gehele behandeling lang?',
-       'Nee:Ja'
-);
-
-insert into Videos(language, topic, title, reference) values (
-       'NL',
-       'Tuberculosis',
-       'Hoe kan ik voorkomen dat ik andere mensen besmet: Hoe en wanneer dragen van een TB masker?',
-       'https://youtu.be/UM-iDAugL1k'
+       'https://youtu.be/eymOMXeSyP4'
 );
 
 insert into Quizzes(video, question, answers) values(
        17,
+       'Duurt de besmettelijke fase de gehele behandeling lang?',
+       'Nee:Ja'
+);
+
+--Maybe remove, dead link?
+insert into Videos(language, topic, title, reference) values (
+       'NL',
+       'Infectie en Preventie',
+       'Hoe kan ik voorkomen dat ik andere mensen besmet:Hoe en wanneer dragen van een TB masker?',
+       'https://youtu.be/UM-iDAugL1k'
+);
+
+insert into Quizzes(video, question, answers) values(
+       18,
        'Wanneer moet ik het masker dragen',
        'Wanneer ik met andere mensen ben (als zij zelf geen masker dragen):Altijd, in mijn eigen kamer en in andere kamers en buiten:Alleen wanneer ik hoest'
 );
 
 insert into Videos(language, topic, title, reference) values (
        'NL',
-       'Tuberculosis',
-       'Hoe kan ik voorkomen dat ik andere mensen besmet: hoe hoest ik veilig?',
-       'https://youtu.be/AS-j9AFkYNA '
+       'Infectie en Preventie',
+       'Hoe kan ik voorkomen dat ik andere mensen besmet:hoe hoest ik veilig?',
+       'https://youtu.be/AS-j9AFkYNA'
 );
 
 insert into Quizzes(video, question, answers) values(
@@ -169,7 +170,7 @@ insert into Quizzes(video, question, answers) values(
 
 insert into Videos(language, topic, title, reference) values (
        'NL',
-       'Tuberculosis',
+       'Ondersteuning',
        'Hoe kan ik iemand helpen die tuberculose heeft?',
        'https://youtu.be/UrPvlTRCvHY'
 );
