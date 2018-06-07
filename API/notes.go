76c12cb23547849a43ac435f1cb3af4919158f50
StenSipma <sten.sipma@ziggo.nl>
2018-06-07 16:40:11 +0200

    Corrected errors in note methods + added documentation

--- API/notes.go
+++ API/notes.go
@@ -5,7 +5,6 @@ import (
 	_ "github.com/go-sql-driver/mysql" // anonymous import
 	"github.com/gorilla/mux"
 	http "net/http"
-	"time"
 )
 
 // CREATE
@@ -45,11 +44,10 @@ func createNote(r *http.Request, ar *APIResponse) {
 
 // RETRIEVE
 func retrieveNotes(r *http.Request, ar *APIResponse) {
-
 	vars := mux.Vars(r)
 	patientID := vars["id"]
 
-	rows, err := db.Query(`SELECT question, day FROM Notes WHERE patient_id = ?`, patientID)
+	rows, err := db.Query(`SELECT id, question, day FROM Notes WHERE patient_id = ?`, patientID)
 	if err != nil {
 		ar.setErrorAndStatus(http.StatusInternalServerError, err, "Unexpected error during query")
 		return
@@ -74,21 +72,6 @@ func retrieveNotes(r *http.Request, ar *APIResponse) {
 	ar.setResponse(notes)
 }
 
-// DELETE
-func deleteNote(r *http.Request, ar *APIResponse) {
-	vars := mux.Vars(r)
-	patientID := vars["id"]
-	noteID := vars["note_id"]
-	_, err := db.Exec("DELETE FROM Notes WHERE id=? and patient_id=?", noteID, patientID)
-	if err != nil {
-		ar.setErrorAndStatus(http.StatusInternalServerError, err, "Unexpected error during query")
-		return
-	}
-
-	ar.StatusCode = http.StatusOK
-
-}
-
 // UPDATE
 func updateNote(r *http.Request, ar *APIResponse) {
 	vars := mux.Vars(r)
@@ -106,7 +89,7 @@ func updateNote(r *http.Request, ar *APIResponse) {
 	_, err = tx.Exec(`UPDATE Notes SET
                           question = ?,
                           day = ?
-                          WHERE id = ?`, note.Note, time.Now(), noteID)
+                          WHERE id = ?`, note.Note, note.CreatedAt, noteID)
 	if err != nil {
 		ar.setErrorAndStatus(http.StatusInternalServerError, err, "Failed to insert note into the database.")
 		return
@@ -116,7 +99,16 @@ func updateNote(r *http.Request, ar *APIResponse) {
 		ar.setErrorAndStatus(http.StatusInternalServerError, err, "Failed to commit changes to database.")
 		return
 	}
+}
 
-	ar.StatusCode = http.StatusCreated
-
+// DELETE
+func deleteNote(r *http.Request, ar *APIResponse) {
+	vars := mux.Vars(r)
+	patientID := vars["id"]
+	noteID := vars["note_id"]
+	_, err := db.Exec("DELETE FROM Notes WHERE id=? and patient_id=?", noteID, patientID)
+	if err != nil {
+		ar.setErrorAndStatus(http.StatusInternalServerError, err, "Unexpected error during query")
+		return
+	}
 }
